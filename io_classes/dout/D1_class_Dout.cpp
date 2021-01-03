8aed076aadfd340cc8d0d11830298bf564599fe6
Karl Hartinger <karl@hartingers.at>
2021-01-03 18:00:07 +0100

    Digital out: #define default values

--- io_classes/dout/D1_class_Dout.cpp
+++ io_classes/dout/D1_class_Dout.cpp
@@ -1,47 +1,46 @@
 //_____D1_class_Dout.cpp______________________170402-200414_____
 // D1 mini class for digital output, e.g. leds.
 // Default: pin D4=GPIO2 (blue led), output not inverted (0->0V)
-//          value logical 0
-// Changes 2017-12-03:
-// + devide (old) D1_class_Dout.h into *.h and *.cpp
-// + processing of invert_ now in on() / off() (instead of set)
-// + toggle(), getDuration(), Dout(bool) added
-//
+//          value logical 0 (=0V, because invert=false)
 // Hardware: (1) WeMos D1 mini
 //
 // Created by Karl Hartinger, December 03, 2017.
 // Changes:
+// * 2017-12-03 devide (old) D1_class_Dout.h into *.h and *.cpp
+//   + process invert_ now in on(), off() (instead of set)
+//   + add toggle(), getDuration(), Dout(bool)
 // * 2020-04-14 add start value to constructor
+// * 2021-01-03 #define default values
 // Released into the public domain.
 
 #include "D1_class_Dout.h"
 
-//**************************************************************
-//    constructor & co
-//**************************************************************
+// *************************************************************
+//       constructor & co
+// *************************************************************
 
-//_____constructor (default digital out is D4, not inverted)____
-Dout::Dout() { setup(D4, false, 0); }
+//_______constructor (default output pin GPIO2=D4, not inverted)
+Dout::Dout(){setup(DEFAULT_PIN, DEFAULT_INVERT, DEFAULT_VALUE);}
 
-//_____constructor with output pin number (=GPIO, not inverted)_
-Dout::Dout(int num) { setup(num, false, 0); }
+//_______constructor with output pin number (=GPIO,not inverted)
+Dout::Dout(int num) {setup(num, DEFAULT_INVERT, DEFAULT_VALUE);}
 
-//_____constructor (default outpin is D4), set invert___________
+//_______constructor (default outpin GPIO2=D4), set invert______
 // example: declare blue led on D1mini: Dout led_blue(true);
-Dout::Dout(bool invert) { setup(D4, invert, 0); }
+Dout::Dout(bool invert){setup(DEFAULT_PIN, invert, DEFAULT_VALUE);}
 
-//_____constructor with pin number and set invert_______________
-Dout::Dout(int num, bool invert) { setup(num, invert, 0); }
+//_______constructor with pin number and set invert_____________
+Dout::Dout(int num, bool invert){setup(num, invert, DEFAULT_VALUE);}
 
-//_____constructor with pin number and start value______________
+//_______constructor with pin number and start value____________
 Dout::Dout(int num, int startvalue)
-{ setup(num, false, startvalue); }
+{ setup(num, DEFAULT_INVERT, startvalue); }
 
-//_____constructor with pin number, set invert and start value__
+//_______constructor with pin number, set invert, start value___
 Dout::Dout(int num, bool invert, int startvalue)
 { setup(num, invert, startvalue); }
 
-//_____setup output pin_________________________________________
+//_______setup output pin_______________________________________
 void Dout::setup(int num, bool invert, int startvalue)
 {
  invert_=invert;                       // 
@@ -57,29 +56,28 @@ void Dout::setup(int num, bool invert, int startvalue)
  set(startvalue);                      // in set ;)
 }
 
-//**************************************************************
-//     setter, getter, working methods
-//**************************************************************
+// *************************************************************
+//       setter, getter, working methods
+// *************************************************************
 
-//_____set digital output pin physical: 0=0V, 1=3V3_____________
+//_______set digital output pin physical: 0=0V, 1=3V3___________
 void Dout::set(int val)
 {
- int vout_;
  if(val==val_) return;                 // nothing to change
  val_=val;                             // save new value
  lastMilli_=millis();                  // new start
  digitalWrite(doutNum_, val_);         // do output
 }
 
-//_____turn digital output logical on (can be 3V3 or 0V)________
+//_______turn digital output logical on (can be 3V3 or 0V)______
 void Dout::on(void)  { if(invert_) set(0); else set(1); }
 
-//_____turn digital output logical off (can be 0V or 3V3)_______
+//_______turn digital output logical off (can be 0V or 3V3)_____
 void Dout::off(void) { if(invert_) set(1); else set(0); }
 
-//_____toggle digital output____________________________________
+//_______toggle digital output__________________________________
 void Dout::toggle(void) { set(1-val_); }
 
-//_____get duration of this signal______________________________
+//_______get duration of this signal____________________________
 unsigned long  Dout::getDuration(void) 
   { return (millis()- lastMilli_); }
